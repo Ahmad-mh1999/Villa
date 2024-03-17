8e3a68f227f8ff0a14ffa2a9270bc638c3d1be8b
Ahmad-mh1999 <ahmedmhmad021@gmail.com>
2024-03-17 20:41:24 +0300

    yml uploud

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter ,Routes ,Route} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/Villa'>
     <React.StrictMode>
       <Routes >
         <Route path='/*' element={<App />} />
